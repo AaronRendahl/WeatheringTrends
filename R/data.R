634553952eea726ecc9e709494da79c6551f709f
Aaron Rendahl <rend0020@umn.edu>
2016-03-11 23:32:20 -0600

    Add example data set

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Example data set
+#'
+#' A data set containing concentrations of several elements over a range of depths.
+#'
+#' @format
+#' \describe{
+#' \item{depth.top}{Depth at the top of the sample}
+#' \item{depth.bottom}{Depth at the bottom of the sample}
+#' \item{...}{Concentrations of various elements}
+#' }
+"welldata"
