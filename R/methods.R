7806513fde5864fed736b7d9926c0f60845c5c01
Aaron Rendahl <arendahl@stat.umn.edu>
2016-03-16 11:09:47 -0500

    Should plot curve even on original scale because +/- SD lines aren't quite straight.

--- R/methods.R
+++ R/methods.R
@@ -24,7 +24,7 @@ fitted.ElementRatio <- function(object, depth=object$data$depth, ...) {
 plot.ElementRatio <- function(x, sd=1, main=paste0(x$mobile, "/", x$immobile), log=FALSE,
                       ylab=if(log) "logratio" else "ratio", ...) {
   depth <- x$data$depth
-  xx <- c(0, seq(x$output[["depth1"]], x$output[["depth2"]], len=if(log) 50 else 2), max(depth))
+  xx <- c(0, seq(x$output[["depth1"]], x$output[["depth2"]], len=50), max(depth))
   x.fit <- fitted.ElementRatio(x, depth=xx)
   tolog <- if(log) identity else exp
   plot(depth, tolog(x$data$logratio), main=main, ylab=ylab, ...)
