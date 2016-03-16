44d3e6ca37024508aedd8118ed4d3116230e81ce
Aaron Rendahl <arendahl@stat.umn.edu>
2016-03-16 10:55:39 -0500

    Add basic vignette.

---
+++ inst/doc/StartingWithWeatheringTrends.R
@@ -0,0 +1,23 @@
+## ------------------------------------------------------------------------
+library(WeatheringTrends)
+data(welldata)
+fit1 <- FitElementRatio("Sr", "Zr", "depth.top", welldata)
+fit1$par
+fit1$output
+plot(fit1)
+
+## ------------------------------------------------------------------------
+fit1 <- profile(fit1, "x2")
+fit1$confint
+
+## ------------------------------------------------------------------------
+fit2 <- FitElementRatio("Zr", "Sr", "depth.top", welldata)
+fit2$par
+fit2$output
+plot(fit2)
+
+## ---- fig.show='hold'----------------------------------------------------
+fits <- FitElementRatios(c("Sr", "Pb"), c("Zr", "V"), "depth.top", welldata)
+par(mfrow=c(2,2))
+for(i in 1:2) for(j in 1:2) plot(fits[[i]][[j]])
+
