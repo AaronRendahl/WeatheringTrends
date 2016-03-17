229c431db102b84b712704e34b64965e60e1cd0c
Aaron Rendahl <arendahl@stat.umn.edu>
2016-03-17 09:20:06 -0500

    add hessian capability to boot function too

--- R/boot.R
+++ R/boot.R
@@ -3,11 +3,13 @@
 #' \code{BootElementRatio}
 #'
 #' @param x the ElementRatio object
+#' @param hessian get the hessian from the optimization
+#' @param ... additional parameters sent to \code{ControlElementRatioFit}
 #' @return a new ElementRatio object, with bootstrapped data
 #' @export
-BootElementRatio <- function(x) {
+BootElementRatio <- function(x, hessian=!is.null(x$optim$hessian), ...) {
   f <- fitted(x)
   di <- data.frame(depth=x$data$depth, logratio=rnorm(nrow(f), mean=f$estimate, sd=f$sd))
-  refit(list(data=di, control=ControlElementRatioFit(di$depth, di$logratio),
-             mobile=x$mobile, immobile=x$immobile))
+  refit(list(data=di, control=ControlElementRatioFit(di$depth, di$logratio, ...),
+             mobile=x$mobile, immobile=x$immobile), hessian=hessian)
 }
