1ade27b614d04488b1e25252d833c899715a6ff0
Aaron Rendahl <arendahl@stat.umn.edu>
2016-03-17 10:38:56 -0500

    make default to have plot rotated

--- R/methods.R
+++ R/methods.R
@@ -16,13 +16,15 @@ fitted.ElementRatio <- function(object, depth=object$data$depth, ...) {
 #' @param x object
 #' @param sd how many sd out to put the lines
 #' @param main title of plot
+#' @param rotate should the plot be rotated to have depth on vertical axis
 #' @param log to put on log scale or not
-#' @param ylab y label
+#' @param ci.lty line type of confidence intervals
+#' @param responselabel label of response axis
 #' @param ... additional parameters sent to plot
 #' @return NULL
 #' @export
-plot.ElementRatio <- function(x, sd=1, main=paste0(x$mobile, "/", x$immobile), log=FALSE,
-                      ylab=if(log) "logratio" else "ratio", ...) {
+plot.ElementRatio <- function(x, sd=1, main=paste0(x$mobile, "/", x$immobile), rotate=TRUE, log=FALSE, ci.lty=3,
+                      responselabel=if(log) "logratio" else "ratio", ...) {
   depth <- x$data$depth
   xx <- c(0, seq(x$output[["depth1"]], x$output[["depth2"]], len=50), max(depth))
   tolog <- if(log) identity else exp
@@ -34,22 +36,41 @@ plot.ElementRatio <- function(x, sd=1, main=paste0(x$mobile, "/", x$immobile), l
   if(!is.null(x$confint)) {
     if("x2" %in% rownames(x$confint)) {
       ci <- x$confint["x2",]
-      d0 <- diff(tolog(range(x$data$logratio)))*0.01
       x0 <- x$output[["depth2"]]
       y0 <- tolog(x$output[["logratio2"]])
-      dx <- diff(grconvertX(c(0, 1), "npc", "inches"))
-      dy <- diff(grconvertY(c(0, 1), "npc", "inches"))
-      len <- sqrt(dx^2 + dy^2) * 0.02
-      cis <- data.frame(x0=ci[1], x1=ci[2], y0=y0 + d0, y1=y0 + d0)
+      cis <- data.frame(x0=ci[1], x1=ci[2], y0=y0, y1=y0, xd=0, yd=0.005)
       rownames(cis) <- "x2"
     }
   }
-  plot(depth, tolog(x$data$logratio), main=main, ylab=ylab, ...)
-  lines(xx, x.fit$estimate)
-  lines(xx, x.fit$lower, lty=2)
-  lines(xx, x.fit$upper, lty=2)
-  if(!is.null(cis)) for(i in seq_len(nrow(cis))) {
-    arrows(x0=cis$x0[1], x1=cis$x1[1], y0=cis$y0[1], y1=cis$y1[1],
-           code=3, angle=90, length=len, lty=3)
+  if(rotate) {
+    plot(tolog(x$data$logratio), depth, main=main, xlab=responselabel, ylim=rev(range(depth)), ...)
+    lines(x.fit$estimate, xx)
+    lines(x.fit$lower, xx, lty=2)
+    lines(x.fit$upper, xx, lty=2)
+    xl <- diff(grconvertX(c(0,1), "npc", "user"))
+    yl <- diff(grconvertY(c(0,1), "npc", "user"))
+    dx <- diff(grconvertX(c(0, 1), "npc", "inches"))
+    dy <- diff(grconvertY(c(0, 1), "npc", "inches"))
+    len <- sqrt(dx^2 + dy^2) * 0.01  
+    if(!is.null(cis)) for(i in seq_len(nrow(cis))) {
+      arrows(y0=cis$x0[i]+cis$xd[i]*yl, y1=cis$x1[i]+cis$xd[i]*yl, 
+             x0=cis$y0[i]+cis$yd[i]*xl, x1=cis$y1[i]+cis$yd[i]*xl,
+             code=3, angle=90, length=len, lty=ci.lty)
+    }
+  } else {
+    plot(depth, tolog(x$data$logratio), main=main, ylab=responselabel, ...)
+    lines(xx, x.fit$estimate)
+    lines(xx, x.fit$lower, lty=2)
+    lines(xx, x.fit$upper, lty=2)
+    xl <- diff(grconvertX(c(0,1), "npc", "user"))
+    yl <- diff(grconvertY(c(0,1), "npc", "user"))
+    dx <- diff(grconvertX(c(0, 1), "npc", "inches"))
+    dy <- diff(grconvertY(c(0, 1), "npc", "inches"))
+    len <- sqrt(dx^2 + dy^2) * 0.01  
+    if(!is.null(cis)) for(i in seq_len(nrow(cis))) {
+      arrows(x0=cis$x0[i]+cis$xd[i]*xl, x1=cis$x1[i]+cis$xd[i]*xl, 
+             y0=cis$y0[i]+cis$yd[i]*yl, y1=cis$y1[i]+cis$yd[i]*yl,
+             code=3, angle=90, length=len, lty=ci.lty)
+    }
   }
 }
